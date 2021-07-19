ad5a5975b55930fa825304d48aa315ea38232d05
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2021-07-19 15:13:41 +0800

    TamperMonkey code
    稼働with
    https://localhost:8080/js/app.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         seek-haven't-like
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.cybozu.cn/k/*
+// @match        https://*.cybozu.com/k/*
+// @require      https://localhost:8080/js/app.js
+// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict'
+    // Your code here...
+})();
